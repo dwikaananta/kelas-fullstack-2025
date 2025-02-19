eb15feb02d7e9ed68ff7fb9054f9a012e84a7762
Dwika Ananta <dwika.ananta.wiratama@gmail.com>
2025-02-19 17:20:30 +0800

    ha

--- src/main.jsx
+++ src/main.jsx
@@ -7,3 +7,16 @@ createRoot(document.getElementById('root')).render(
     <h1>Hello world!</h1>
   </StrictMode>,
 )
+
+const App = () => {
+  return (
+    <nav>
+      <h1>App Name</h1>
+      <div>
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Login</a>
+      </div>
+    </nav>
+  )
+}
