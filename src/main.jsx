880c04e19ccee0c619410a314c61292f3ac23149
Dwika Ananta <dwika.ananta.wiratama@gmail.com>
2025-02-19 17:45:08 +0800

    ha

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <h1>Hello world!</h1>
-  </StrictMode>,
-)
-
 const App = () => {
   return (
     <nav>
@@ -20,3 +14,24 @@ const App = () => {
     </nav>
   )
 }
+
+const Content = ({ children, title }) => {
+  return (
+    <div className='bg-zinc-700 text-white rounded'>
+      <h1 className='text-2xl'>{title}</h1>
+      {children}
+    </div>
+  )
+}
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+    <Content title="Title for content 1">
+      Children of content 1
+    </Content>
+    <Content title="Title for content 2">
+      Children of content 2
+    </Content>
+  </StrictMode>,
+)
